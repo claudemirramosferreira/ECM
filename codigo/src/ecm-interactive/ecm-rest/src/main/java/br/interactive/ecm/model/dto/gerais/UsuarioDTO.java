43d10d68ae564b00684e83e22cbe855bb7c3298f
Claudemir Ramos Ferreira <claudemirramosferreira@gmail.com>
2015-09-19 18:36:54 -0400

    add

---
+++ codigo/src/ecm-interactive/ecm-rest/src/main/java/br/interactive/ecm/model/dto/gerais/UsuarioDTO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.interactive.ecm.model.dto.gerais;
+
+/**
+ *
+ * @author claudemir
+ */
+public class UsuarioDTO {
+    
+    private String userName;
+    
+    private String firstName;
+    
+    private String lastName;
+    
+    private String email;
+    ~
+    
+    
+}
